9e38c5aba686cc1a7e964f69f788204ee47e94d1
Arthur Sven Msiska <arthursvenmsiska@gmail.com>
2023-11-25 17:37:03 +0200

    clienthome page just getting started

--- app/src/main/java/com/devapps/pamwezi/presentation/MainActivity.kt
+++ app/src/main/java/com/devapps/pamwezi/presentation/MainActivity.kt
@@ -27,6 +27,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -238,7 +239,7 @@ class MainActivity : ComponentActivity() {
                 containerColor = Color.White,
                 contentColor = Color.White,
                 topBar = {
-                    TopAppBar(
+                    CenterAlignedTopAppBar(
                         title = {
                             Text(
                                 text = "Pa Mwezy",
@@ -473,7 +474,7 @@ class MainActivity : ComponentActivity() {
                 containerColor = Color.White,
                 contentColor = Color.White,
                 topBar = {
-                    TopAppBar(
+                    CenterAlignedTopAppBar(
                         title = {
                             Text(
                                 text = "Pa Mwezy",
--- app/src/main/java/com/devapps/pamwezi/presentation/ui/Screens/HomeScreen.kt
+++ app/src/main/java/com/devapps/pamwezi/presentation/ui/Screens/HomeScreen.kt
@@ -1,8 +1,100 @@
 package com.devapps.pamwezi.presentation.ui.Screens
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
+Surface {
+    Scaffold(
+        containerColor = Color.White,
+        contentColor = Color.White,
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text(text = "Pa Mwezy",
+                        fontWeight = FontWeight.Bold,
+                        fontSize = 26.sp,
+                        textAlign = TextAlign.Center,
+                        color = Color.Black)
+                },
+                actions = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(
+                            imageVector = Icons.Filled.MoreVert,
+                            contentDescription = "home menu",
+                            tint = Color.Black)
+                    }
+                }
+                )
+        },
+        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = {  },
+                contentColor = Color.White,
+                containerColor = Color.Black
+                ) {
+                Icon(
+                    imageVector = Icons.Filled.Create,
+                    contentDescription = "Add expense",
+                    )
+                Text(text = "Add Expense")
+            }
+        }
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(it)
+                .background(color = Color.LightGray)
+                .fillMaxSize()
+        ) {
 
+        }
+    }
+}
+}
+
+@Composable
+fun UserBar() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 30.dp)
+    ) {
+
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun showClientHomePreview() {
+    HomeScreen()
 }
