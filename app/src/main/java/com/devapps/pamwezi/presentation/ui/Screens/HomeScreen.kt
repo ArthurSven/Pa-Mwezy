0670d9261fcdc9c58007348aa39d7b41ec532f17
Arthur Sven Msiska <arthursvenmsiska@gmail.com>
2023-12-10 23:53:22 +0200

    Budget card item created

--- app/src/main/java/com/devapps/pamwezi/presentation/ui/Screens/HomeScreen.kt
+++ app/src/main/java/com/devapps/pamwezi/presentation/ui/Screens/HomeScreen.kt
@@ -2,19 +2,26 @@ package com.devapps.pamwezi.presentation.ui.Screens
 
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
 import androidx.compose.material3.FabPosition
@@ -29,8 +36,10 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -184,11 +193,52 @@ fun HomeContent(
 
 @Composable
 fun BudgetListCard() {
-
+ElevatedCard(
+    shape = RoundedCornerShape(10.dp),
+    colors = CardDefaults.elevatedCardColors(
+        containerColor = Color.Black,
+        contentColor = Color.White
+    ),
+    modifier = Modifier
+        .fillMaxWidth()
+        .height(140.dp)
+        .padding(all = 20.dp)
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(all = 10.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(0.90f)
+                .padding(top = 10.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "January Budget",
+                color = Color.White, fontSize = 16.sp)
+            Text(text = "MWK 621099.0",
+                color = Color.White, fontSize = 20.sp,
+                fontWeight = FontWeight.Bold)
+        }
+        Column(
+            modifier = Modifier
+                .weight(0.10f)
+                .padding(top = 25.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Delete,
+                contentDescription = "delete budget")
+        }
+    }
+}
 }
 
 @Composable
 @Preview(showBackground = true)
 fun showBudgetItem() {
-
+    BudgetListCard()
 }
